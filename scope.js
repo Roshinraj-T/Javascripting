82ce6cf8bc60d834b8bbba07e72a8ded8f5b65ac
Roshinraj-T <roshinraj432@gmail.com>
2023-04-21 00:17:24 +0530

    added all js files

--- scope.js
+++ scope.js
@@ -5,8 +5,16 @@ const a = 1; const b = 2; const c = 3;
        
        (function secondFunction () {  
          const b = 8;  
-       console.log('a: '+a+', b: '+b+', c: '+c) 
+       console.log('a: '+a+', b: '+b+', c: '+c);
+         (function thirdFunction () {  
+           const a = 7; const c = 9;  
+       
+           (function fourthFunction () {  
+             const a = 1; const c = 8; 
+              
+           })()  
+         })()  
        })()  
      })() 
-    
+     fourthFunction()
      
